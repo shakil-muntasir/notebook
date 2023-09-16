d78704672dad2da38c756aca2fc728e620641059
Muntasir Rahman <shakil_muntasir@hotmail.com>
2023-09-16 22:25:13 +0600

    Removed reverse proxy configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import 'express-async-errors'
-import { NextFunction, Request, Response, Router } from 'express'
+import { Router } from 'express'
 import swaggerUi from 'swagger-ui-express'
 
 import env from '@/utils/env'
@@ -19,24 +19,12 @@ routes.use('/users', userRoutes)
 
 routes.use('/notes', noteRoutes)
 
-if (['staging', 'production'].includes(env.NODE_ENV)) {
-    /**
-        For reverse proxy, e.g. nginx:
-        location /api/documentation/ {
-            proxy_pass http://<ip>:<port>/api/documentation/;
-        }
-     */
+if (env.NODE_ENV !== 'production') {
+    const swaggerOptions = { swaggerOptions: { persistAuthorization: true } }
 
-    const forwardedPrefixSwagger = async (request: Request, _response: Response, next: NextFunction) => {
-        request.originalUrl = (request.headers['x-forwarded-prefix'] || '') + request.url
-        next()
-    }
-
-    routes.use('/documentation/', forwardedPrefixSwagger, swaggerUi.serve)
-} else {
-    routes.use('/documentation/', swaggerUi.serve)
+    routes.use('/documentation', swaggerUi.serve)
+    routes.get('/documentation', swaggerUi.setup(swaggerDocument, swaggerOptions))
 }
-routes.get('/documentation/', swaggerUi.setup(swaggerDocument, { swaggerOptions: { persistAuthorization: true } }))
 
 routes.use(errorHandler)
 
